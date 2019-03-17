b9245d734f82d69c6a0583ec3c9e593a10a02cc6
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 12:46:14 -0700

    read/write query options added to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import loadMovieCards from './movies/movie-cards-component.js';
 import makeSearchAPIUrl from './movies/make-search-api-url.js';
 import loadSearch from './movies/search-component.js';
+import { writeOptionsAsQuery, readQueryAsOptions } from './query-options.js';
 
-loadSearch(searchOptions => {
-    const url = makeSearchAPIUrl(searchOptions);
+
+let searchOptions = null;
+
+loadSearch(newSearchOptions => {
+    searchOptions = newSearchOptions;
+    const queryOptions = {
+        search: searchOptions
+    };
+
+    const query = writeOptionsAsQuery(queryOptions);
+    window.location.hash = query;
+
+});
+
+window.addEventListener('hashchange', () => {
+    const query = window.location.hash.slice(1);
+    const queryOptions = readQueryAsOptions(query);
+
+    const url = makeSearchAPIUrl(queryOptions);
 
     fetch(url)
         .then(response => response.json())
