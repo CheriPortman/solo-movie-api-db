8d2491d3764ce4da50494403fd2bb22749cc86b7
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 13:49:51 -0700

    paging functions as it should

--- src/index.js
+++ src/index.js
@@ -1,41 +1,63 @@
 import loadMovieCards from './movies/movie-cards-component.js';
+import loadPaging, { updatePagingInfo } from './paging-component.js';
 import makeSearchAPIUrl from './movies/make-search-api-url.js';
 import loadSearch, { updateSearchTerm } from './movies/search-component.js';
 import { writeOptionsAsQuery, readQueryAsOptions } from './query-options.js';
 
 
 let searchOptions = null;
+let pagingOptions = null;
+
+function writeQueryOptionsToHash() {
+    const queryOptions = {
+        search: searchOptions,
+        paging: pagingOptions
+    };
+    const query = writeOptionsAsQuery(queryOptions);
+    window.location.hash = query;
+}
 
 loadSearch(newSearchOptions => {
     searchOptions = newSearchOptions;
-    const queryOptions = {
-        search: searchOptions
-    };
-
-    const query = writeOptionsAsQuery(queryOptions);
-    window.location.hash = query;
+    pagingOptions = { page: 1 };
+    writeQueryOptionsToHash();
 });
 
-runSearchFromQuery();
+loadPaging(newPagingOptions => {
+    pagingOptions = newPagingOptions;
+    writeQueryOptionsToHash();
+});
 
 window.addEventListener('hashchange', () => {
     runSearchFromQuery();
 });
 
+//run on load!
+runSearchFromQuery();
+
 function runSearchFromQuery() {
     const query = window.location.hash.slice(1);
     const queryOptions = readQueryAsOptions(query);
     
+    searchOptions = queryOptions.search;
+    pagingOptions = queryOptions.paging;
+
     const url = makeSearchAPIUrl(queryOptions);
     
-    updateSearchTerm(queryOptions.search);
+    updateSearchTerm(searchOptions);
     if(!url) {
         return;
+        //TODO: clear movies component
     }
 
     fetch(url)
         .then(response => response.json())
         .then(response => {
             loadMovieCards(response.results);
+            const pagingInfo = {
+                page: response.page,
+                totalPages: response.total_pages,
+            };
+            updatePagingInfo(pagingInfo);
         });
 }
--- src/paging-component.js
+++ src/paging-component.js
@@ -1,38 +1,46 @@
+const pagingSection = document.getElementById('paging-section');
 const previousButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
 const currentPage = document.getElementById('current-page');
 const totalPages = document.getElementById('total-pages');
 
 const PER_PAGE = 20;
+let totalPageCount = 0;
+let currentPageNumber = 0;
 
-export default function loadPaging(totalCount, callback) {
-    const totalPageCount = Math.ceil(totalCount / PER_PAGE);
+
+export function updatePagingInfo(pagingInfo) {
+    pagingSection.classList.remove('hidden');
+    totalPageCount = pagingInfo.totalPages;
+    currentPageNumber = pagingInfo.page;
+    currentPage.textContent = currentPageNumber;
+    nextButton.disabled = currentPageNumber === totalPageCount;
+    previousButton.disabled = currentPageNumber === 1;
     totalPages.textContent = totalPageCount;
+}
+
+export default function loadPaging(callback) {
     
-    let currentPageNumber = 1;
-
-    updatePaging();
-
-    function updatePaging() {
-        currentPage.textContent = currentPageNumber;
-        nextButton.disabled = currentPageNumber === totalPageCount;
-        previousButton.disabled = currentPageNumber === 1;
-
+    function callPagingCallback() {
         const pagingOptions = {
             page: currentPageNumber,
             perPage: PER_PAGE
         };
-
-        callback(pagingOptions);  //so we're passing pagingOptions up to index.js
+        
+        callback(pagingOptions);  //so we're passing pagingOptions up to index.js via butten event listener?
     }
 
     nextButton.addEventListener('click', () => {
         currentPageNumber++;
-        updatePaging();
+        callPagingCallback();
     });
-
+    
     previousButton.addEventListener('click', () => {
         currentPageNumber--;
-        updatePaging();
+        callPagingCallback();
     });
+        
 }
+    
+
+// const totalPageCount = Math.ceil(totalCount / PER_PAGE);
