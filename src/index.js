1987314ec0095f41c3db723d654c8615b8b00829
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 09:57:09 -0700

    paging-component.js written, not hooked up yet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import loadMovieCards from '../movies/movie-cards-component.js';
+import loadMovieCards from './movies/movie-cards-component.js';
 
 const API_KEY = '869a081d5dda9c543281108989ab148e';
-const query = encodeURIComponent('Honeymoon');
+const query = encodeURIComponent('Lord of the Rings');
 const page = 1;
 const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`;
 
