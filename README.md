bfa7eab18cdb8516c699fa274ee2c7095b38794d
Cheri Portman <cheriportman@hotmail.com>
2019-03-16 19:39:09 -0700

    makeMovieCard test passes with interpolated data

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 ### * example API Request:  https://api.themoviedb.org/3/movie/550?api_key=869a081d5dda9c543281108989ab148e
 ### * API ReadAccess Token (v4 auth):  eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjlhMDgxZDVkZGE5YzU0MzI4MTEwODk4OWFiMTQ4ZSIsInN1YiI6IjVjOGQzYTY1OTI1MTQxMGZlNzlmMjg1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QeNm0pN2RH7QMKmD_Hv_KizbxL_dcDHtIvIbT2ZX6zI
 
+troubles:
+getting movie.genres.name (undefined. I can get movie.genres object, but can't drill into it...)
+
+overview for the title attribute. adds a bunch of characters?
--- test/movies-component.test.js
+++ test/movies-component.test.js
@@ -1,16 +1,18 @@
+import movie from '../data/example-data.js';
 const test = QUnit.test;
 
 QUnit.module('makeMovieCard component:');
 
-function makeMovieCard() {
+function makeMovieCard(movie) {
+
     const html = /*html*/`
-        <li class="movie" title="A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground &quot;fight clubs&quot; forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.">
-            <p class="genre">Drama</p>
-            <h2 class="movie-title">Fight Club</h2>
-            <img src="https://image.tmdb.org/t/p/w92/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg" alt="poster of Fight Club">
-            <span class="tagline">Mischief. Mayhem. Soap.</span>
-            <span class="year">1999</span>
-            <span class="runtime">139 minutes</span>
+        <li class="movie">
+            <h2 class="movie-title">${movie.original_title}</h2>
+            <img src="https://image.tmdb.org/t/p/w92${movie.poster_path}" 
+                alt="poster of ${movie.original_title}">
+            <span class="tagline">${movie.tagline}</span>
+            <span class="year">${movie.release_date.split('-')[0]}</span>
+            <span class="runtime">${movie.runtime} minutes</span>
         </li>
     `;
 
@@ -22,8 +24,7 @@ function makeMovieCard() {
 test('write a test', assert => {
     //arrange
     const expected = /*html*/`
-        <li class="movie" title="A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground &quot;fight clubs&quot; forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.">
-            <p class="genre">Drama</p>
+        <li class="movie">
             <h2 class="movie-title">Fight Club</h2>
             <img src="https://image.tmdb.org/t/p/w92/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg" alt="poster of Fight Club">
             <span class="tagline">Mischief. Mayhem. Soap.</span>
@@ -31,27 +32,8 @@ test('write a test', assert => {
             <span class="runtime">139 minutes</span>
         </li>
     `;
-
-    const movie = {
-        'genres': [
-            {
-                'id': 18,
-                'name': 'Drama'
-            }
-        ],
-        'id': 550,
-        'imdb_id': 'tt0137523',
-        'original_title': 'Fight Club',
-        'overview': 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
-        'poster_path': '/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg',
-        'release_date': '1999-10-15',
-        'runtime': 139,
-        'tagline': 'Mischief. Mayhem. Soap.',
-        'title': 'Fight Club'
-    };
-
     //act
-    const result = makeMovieCard();
+    const result = makeMovieCard(movie);
     //assert
     assert.htmlEqual(result, expected);
 });
