16bcb84210f66d7f88f22246adaa51ba1a3e75f6
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 12:12:07 -0700

    renamed pieces of makeSearchAPIUrl

--- src/movies/make-search-api-url.js
+++ src/movies/make-search-api-url.js
@@ -1,6 +1,7 @@
 const API_KEY = '869a081d5dda9c543281108989ab148e';
 
-export default function makeSearchAPIURL(searchOptions) {
+export default function makeSearchAPIUrl(queryOptions) {
+    const searchOptions = queryOptions.search;
     const query = encodeURIComponent(searchOptions.term);
     const page = 1;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`;
--- test/make-search-api-url.test.js
+++ test/make-search-api-url.test.js
@@ -4,13 +4,13 @@ const test = QUnit.test;
 QUnit.module('make search url');
 
 test('includes encoded search term', assert => {
-    const searchOptions = {
-        term: 'star wars'
+    const queryOptions = {
+        search: { term: 'star wars' }
     };
 
     const expected = 'https://api.themoviedb.org/3/search/movie?api_key=869a081d5dda9c543281108989ab148e&language=en-US&query=star%20wars&page=1&include_adult=false';
 
-    const result = makeSearchAPIUrl(searchOptions);
+    const result = makeSearchAPIUrl(queryOptions);
 
     assert.equal(result, expected);
 });
