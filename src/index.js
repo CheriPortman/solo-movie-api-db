8736e85dcd1725ebe4a4a344766a079d2c84447c
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 14:55:15 -0700

    index.js refactored, cleaner code

--- src/index.js
+++ src/index.js
@@ -4,47 +4,39 @@ import makeSearchAPIUrl from './movies/make-search-api-url.js';
 import loadSearch, { updateSearchTerm } from './movies/search-component.js';
 import { writeOptionsAsQuery, readQueryAsOptions } from './query-options.js';
 
+let queryOptions = null;
 
-let searchOptions = null;
-let pagingOptions = null;
+loadSearch(searchOptions => {
+    queryOptions.search = searchOptions;
+    queryOptions.paging = { page: 1 };
+    writeQueryOptionsToHash();
+});
+
+loadPaging(pagingOptions => {
+    queryOptions.paging = pagingOptions;
+    writeQueryOptionsToHash();
+});
 
 function writeQueryOptionsToHash() {
-    const queryOptions = {
-        search: searchOptions,
-        paging: pagingOptions
-    };
     const query = writeOptionsAsQuery(queryOptions);
     window.location.hash = query;
 }
 
-loadSearch(newSearchOptions => {
-    searchOptions = newSearchOptions;
-    pagingOptions = { page: 1 };
-    writeQueryOptionsToHash();
-});
-
-loadPaging(newPagingOptions => {
-    pagingOptions = newPagingOptions;
-    writeQueryOptionsToHash();
-});
+//run on load!
+runSearchFromQuery();
 
 window.addEventListener('hashchange', () => {
     runSearchFromQuery();
 });
 
-//run on load!
-runSearchFromQuery();
 
 function runSearchFromQuery() {
     const query = window.location.hash.slice(1);
-    const queryOptions = readQueryAsOptions(query);
+    queryOptions = readQueryAsOptions(query);
     
-    searchOptions = queryOptions.search;
-    pagingOptions = queryOptions.paging;
-
+    updateSearchTerm(queryOptions.search);
     const url = makeSearchAPIUrl(queryOptions);
-    
-    updateSearchTerm(searchOptions);
+
     if(!url) {
         return;
         //TODO: clear movies component
