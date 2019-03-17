c629032566111e57000e7568698e81aa1a254348
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 16:08:47 -0700

    makeSearchMovieUrl in own module

--- src/movies/make-search-api-url.js
+++ src/movies/make-search-api-url.js
@@ -1,7 +1,7 @@
 const SEARCH_MOVIE_URL = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = '869a081d5dda9c543281108989ab148e';
 
-export default function makeSearchMovieUrl(queryOptions) {
+export function makeSearchMovieUrl(queryOptions) {
     const url = new URL(SEARCH_MOVIE_URL);
     url.searchParams.set('api_key', API_KEY);
     url.searchParams.set('language', 'en-us');
