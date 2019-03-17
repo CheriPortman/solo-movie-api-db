513f1d0beb5d5462ac25145dc52c79620285d0a1
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 16:07:32 -0700

    makeSearchMovieUrl test passes

--- src/movies/make-search-api-url.js
+++ src/movies/make-search-api-url.js
@@ -1,21 +1,13 @@
 const SEARCH_MOVIE_URL = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = '869a081d5dda9c543281108989ab148e';
 
-const url = new URL(SEARCH_MOVIE_URL);
-url.searchParams.set('api_key', API_KEY);
-url.searchParams.set('language', 'en-us');
-url.searchParams.set('include_adult', false);
-
-export default function makeSearchAPIUrl(queryOptions) {
-    const searchOptions = queryOptions.search;
-    const searchTerm = searchOptions.term;
-    url.searchParams.set('query', searchTerm);
-    
-    const pagingOptions = queryOptions.paging;
-    const page = pagingOptions ? pagingOptions.page : 1;
-    url.searchParams.set('page', page);
-
-    return searchTerm ? url.toString() : '';
+export default function makeSearchMovieUrl(queryOptions) {
+    const url = new URL(SEARCH_MOVIE_URL);
+    url.searchParams.set('api_key', API_KEY);
+    url.searchParams.set('language', 'en-us');
+    url.searchParams.set('include_adult', false);
+    url.searchParams.set('query', queryOptions.searchTerm);
+    url.searchParams.set('page', queryOptions.page);
+    return url.toString();
 }
-
 
--- test/make-search-api-url.test.js
+++ test/make-search-api-url.test.js
@@ -1,14 +1,24 @@
-// import makeSearchAPIUrl from '../src/movies/make-search-api-url.js';
+import makeSearchMovieUrl from '../src/movies/make-search-api-url.js';
 
-// const test = QUnit.test;
-// QUnit.module('make search url');
+const test = QUnit.test;
+QUnit.module('make search movie url');
+
+test('includes query and page', assert => {
+    //arrange
+    const queryOptions = {
+        searchTerm: 'star wars',
+        page: 3
+    };
+    const expected = 'https://api.themoviedb.org/3/search/movie?api_key=869a081d5dda9c543281108989ab148e&language=en-us&include_adult=false&query=star+wars&page=3';
+    const url = makeSearchMovieUrl(queryOptions);
+    assert.equal(url, expected);
+});
 
 // test('includes encoded search term, defaults to page 1', assert => {
 //     const queryOptions = {
 //         search: { term: 'star wars' }
 //     };
 
-//     const expected = 'https://api.themoviedb.org/3/search/movie?api_key=869a081d5dda9c543281108989ab148e&language=en-us&include_adult=false&query=star+wars&page=1';
 
 //     const result = makeSearchAPIUrl(queryOptions);
 
