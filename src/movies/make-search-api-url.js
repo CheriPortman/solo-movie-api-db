802b0a418f7c39005a3dd3537bfdb15033b03adf
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 14:20:19 -0700

    refactored makeSearchAPIUrl, test passes

--- src/movies/make-search-api-url.js
+++ src/movies/make-search-api-url.js
@@ -1,17 +1,23 @@
+const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie";
 const API_KEY = '869a081d5dda9c543281108989ab148e';
+
+const url = new URL(SEARCH_MOVIE_URL);
+url.searchParams.set('api_key', API_KEY);
+url.searchParams.set('language', 'en-us');
+url.searchParams.set('include_adult', false);
 
 export default function makeSearchAPIUrl(queryOptions) {
     const searchOptions = queryOptions.search;
-
     const searchTerm = searchOptions.term;
     if(!searchTerm) return '';
 
+    url.searchParams.set('query', searchOptions.term);
     const pagingOptions = queryOptions.paging;
 
-    const query = encodeURIComponent(searchOptions.term);
     const page = pagingOptions ? pagingOptions.page : 1;
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`;
-    return url;
+    url.searchParams.set('page', page);
+
+    return url.toString();
 }
 
 
--- test/make-search-api-url.test.js
+++ test/make-search-api-url.test.js
@@ -8,7 +8,7 @@ test('includes encoded search term, defaults to page 1', assert => {
         search: { term: 'star wars' }
     };
 
-    const expected = 'https://api.themoviedb.org/3/search/movie?api_key=869a081d5dda9c543281108989ab148e&language=en-US&query=star%20wars&page=1&include_adult=false';
+    const expected = 'https://api.themoviedb.org/3/search/movie?api_key=869a081d5dda9c543281108989ab148e&language=en-us&include_adult=false&query=star+wars&page=1';
 
     const result = makeSearchAPIUrl(queryOptions);
 
