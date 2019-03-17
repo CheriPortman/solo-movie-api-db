f3ef8cffe644489635ce3ee07f67a104da56fe7b
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 15:25:53 -0700

    writeSearchToQuery 1st test passes

---
+++ test/hash-query.test.js
@@ -0,0 +1,20 @@
+const test = QUnit.test;
+QUnit.module('hash query');
+
+function writeSearchToQuery(existingQuery, searchTerm) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('searchTerm', searchTerm);
+    searchParams.set('page', 1);
+    return searchParams.toString();
+}
+
+test ('write search to empty query', assert => {
+    //arrange
+    const existingQuery = '';
+    const searchTerm = 'star wars';
+    //act
+    const query = writeSearchToQuery(existingQuery, searchTerm);
+    //assert
+    assert.equal(query, 'searchTerm=star+wars&page=1');
+
+});
