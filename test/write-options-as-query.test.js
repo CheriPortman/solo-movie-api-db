d6e5eda24231b1e73cfb45b621c84e5ee4f0339c
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 11:37:34 -0700

    writeOptionsAsQuery passes test with paging options

--- test/write-options-as-query.test.js
+++ test/write-options-as-query.test.js
@@ -3,13 +3,16 @@
 const test = QUnit.test;
 QUnit.module('write options to hash');
 
-function writeOptionsAsQuery(searchOptions) {
+function writeOptionsAsQuery(searchOptions, pagingOptions) {
     if(!searchOptions.term) {
         return '';
     }
 
     const searchParams = new URLSearchParams();
     searchParams.set('searchTerm', searchOptions.term);
+    if(pagingOptions) {
+        searchParams.set('page', pagingOptions.page);
+    }
 
     return '?' + searchParams.toString();
 }
@@ -17,11 +20,18 @@ function writeOptionsAsQuery(searchOptions) {
 test('makes query search term', assert => {
     //arrange
     const searchOptions = { term: 'star wars' };
-    
     //act
     const query = writeOptionsAsQuery(searchOptions);
-
     //assert
     assert.equal(query, '?searchTerm=star+wars');
+});
+
+test('makes query for search and paging', assert => {
+    const searchOptions = { term: 'star wars' };
+    const pagingOptions = { page: 1 };
+    //act
+    const query = writeOptionsAsQuery(searchOptions, pagingOptions);
+
+    assert.equal(query, '?searchTerm=star+wars&page=1');
 
 });
