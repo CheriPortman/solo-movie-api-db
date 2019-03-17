65f31d093216e607194b4264dca5c95c1e2641d8
Cheri Portman <cheriportman@hotmail.com>
2019-03-16 19:51:59 -0700

    makeMovieCard moved to movie-cards-component.js

---
+++ movies/movie-cards-component.js
@@ -0,0 +1,17 @@
+export function makeMovieCard(movie) {
+    const html = /*html*/`
+        <li class="movie">
+            <h2 class="movie-title">${movie.original_title}</h2>
+            <img src="https://image.tmdb.org/t/p/w92${movie.poster_path}" 
+                alt="poster of ${movie.original_title}">
+            <span class="tagline">${movie.tagline}</span>
+            <span class="year">${movie.release_date.split('-')[0]}</span>
+            <span class="runtime">${movie.runtime} minutes</span>
+        </li>
+    `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
--- test/movies-component.test.js
+++ test/movies-component.test.js
@@ -1,27 +1,11 @@
 import movie from '../data/example-data.js';
+import { makeMovieCard } from '../movies/movie-cards-component.js';
+
 const test = QUnit.test;
 
 QUnit.module('makeMovieCard component:');
 
-function makeMovieCard(movie) {
-
-    const html = /*html*/`
-        <li class="movie">
-            <h2 class="movie-title">${movie.original_title}</h2>
-            <img src="https://image.tmdb.org/t/p/w92${movie.poster_path}" 
-                alt="poster of ${movie.original_title}">
-            <span class="tagline">${movie.tagline}</span>
-            <span class="year">${movie.release_date.split('-')[0]}</span>
-            <span class="runtime">${movie.runtime} minutes</span>
-        </li>
-    `;
-
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
-
-test('write a test', assert => {
+test('makeMovieCard', assert => {
     //arrange
     const expected = /*html*/`
         <li class="movie">
