c85912755ed216009d025807d50aaf6f9d91a976
Cheri Portman <cheriportman@hotmail.com>
2019-03-17 12:22:05 -0700

    readQueryAsOptions passes test: search and paging

--- test/query-options.test.js
+++ test/query-options.test.js
@@ -3,6 +3,18 @@ import { writeOptionsAsQuery } from '../src/query-options.js';
 const test = QUnit.test;
 QUnit.module('write options to hash');
 
+function readQueryAsOptions(query) {
+    const searchParams = new URLSearchParams(query);
+    const searchTerm = searchParams.get('searchTerm');
+    const page = parseInt(searchParams.get('page'));
+
+    const queryOptions = {
+        search: { term: searchTerm || '' },
+        paging: { page: page || 1 }
+    };
+    return queryOptions;
+}
+
 test('makes query search term', assert => {
     //arrange
     const searchOptions = { term: 'star wars' };
@@ -20,4 +32,17 @@ test('makes query for search and paging', assert => {
 
     assert.equal(query, '?searchTerm=star+wars&page=1');
 
+});
+
+test('reads query with search term and page', assert => {
+    //arrange
+    const query = '?searchTerm=star+wars&page=1';
+    const expected = {
+        search: { term: 'star wars' },
+        paging: { page: 1 }
+    };
+    //act
+    const result = readQueryAsOptions(query);
+
+    assert.deepEqual(result, expected);
 });
